72da615e77b3f42e4dba99b3afbeb3d1572abc00
AGebn <adam.gebner@protonmail.com>
2023-03-24 15:10:04 -0700

    express server almost working

--- expressServer.js
+++ expressServer.js
@@ -1,18 +1,48 @@
+//dependencies
 const express = require("express");
+const fs = require("fs");
 const app = express();
-const port = process.env.PORT || 3000;
-const listener = () => `Listening on port ${port}!`;
-app.listen(port);
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Pet Shop");
+  console.log("test");
+});
 
 app.get("/pets", (req, res) => {
-  console.log("test");
-  res.status(200).json({ message: "/pets.json" });
+  fs.readFile("pets.json", "utf-8", (errors, pets) => {
+    if (error) {
+      res.error(error);
+    } else {
+      res.json(JSON.parse(pets));
+      res.status(200);
+      res.end();
+    }
+  });
+  // console.log("test");
+  // res.status(200).json({ message: "/pets.json" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ error: err });
+app.get("/pets/:petId", (req, res) => {
+  fs.readFile("pets.json", "utf-8", (errors, petsString) => {
+    if (error) {
+      res.sendStatus(500);
+      res.end();
+    } else {
+      let pets = JSON.parse(petsString);
+      if (!pets[req.params.petId]) {
+        res.sendStatus(404, "not found");
+      } else {
+        res.json(pets[req.params.petId]);
+        res.status(200);
+        res.end();
+      }
+    }
+  });
+  // console.log("test");
+  // res.status(200).json({ message: "/pets.json" });
 });
 
-app.use((req, res, next) => {
-  res.status(404).json({ error: { message: "Not found" } });
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 });
