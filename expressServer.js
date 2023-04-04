a52853287b2464555a3cc16bb97bb9511e6621ee
AGebn <adam.gebner@protonmail.com>
2023-04-04 13:07:29 -0700

    starting rest

--- expressServer.js
+++ expressServer.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = process.env.port || 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Pet Shop");
